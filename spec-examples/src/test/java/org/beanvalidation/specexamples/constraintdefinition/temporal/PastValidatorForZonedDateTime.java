10057e9385bcdfd0024401f579da4243287a413f
Gunnar Morling <gunnar.morling@googlemail.com>
2017-01-09 16:12:41 +0100

    BVAL-496 Adding notion of clock provider

---
+++ spec-examples/src/test/java/org/beanvalidation/specexamples/constraintdefinition/temporal/PastValidatorForZonedDateTime.java
@@ -0,0 +1,36 @@
+/*
+ * Hibernate Validator, declare and validate application constraints
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
+package org.beanvalidation.specexamples.constraintdefinition.temporal;
+
+import java.time.ZonedDateTime;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import javax.validation.constraints.Past;
+
+//tag::include[]
+/**
+ * Validates that the given {@link ZonedDateTime} is in the past.
+ */
+public class PastValidatorForZonedDateTime implements ConstraintValidator<Past, ZonedDateTime> {
+
+	@Override
+	public void initialize(Past constraintAnnotation) {
+	}
+
+	@Override
+	public boolean isValid(ZonedDateTime value, ConstraintValidatorContext context) {
+		if ( value == null ) {
+			return true;
+		}
+
+		ZonedDateTime now = ZonedDateTime.now( context.getClockProvider().getClock() );
+
+		return value.isBefore( now );
+	}
+}
+//end::include[]
