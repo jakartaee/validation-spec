8429127f8f52c6b299d7628d053ab7b5771a7186
Guillaume Smet <guillaume.smet@gmail.com>
2017-04-20 17:37:05 +0200

    BVAL-584 Use the shorter license header
    And add the license-maven-plugin to the Maven projects.

--- spec-examples/src/test/java/org/beanvalidation/specexamples/constraintdefinition/temporal/PastValidatorForZonedDateTime.java
+++ spec-examples/src/test/java/org/beanvalidation/specexamples/constraintdefinition/temporal/PastValidatorForZonedDateTime.java
@@ -1,5 +1,5 @@
 /*
- * Hibernate Validator, declare and validate application constraints
+ * Bean Validation: constrain once, validate everywhere.
  *
  * License: Apache License, Version 2.0
  * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
--- spec-examples/src/test/java/org/beanvalidation/specexamples/constraintmetadata/ValidInterval.java
+++ spec-examples/src/test/java/org/beanvalidation/specexamples/constraintmetadata/ValidInterval.java
@@ -1,3 +1,9 @@
+/*
+ * Bean Validation: constrain once, validate everywhere.
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
 package org.beanvalidation.specexamples.constraintmetadata;
 
 import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
