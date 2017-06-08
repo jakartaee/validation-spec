9f200e654b7a28face8e7c9424742a2efcef170c
Gunnar Morling <gunnar.morling@googlemail.com>
2017-06-08 10:23:29 +0200

    BVAL-639 Misc. improvements as per Emmanuel's review:
    * Improving constraint validator example
    * Claryifing value extractor examples
    * Breaking up paragraph on types used for validator resolution

---
+++ spec-examples/src/test/java/org/beanvalidation/specexamples/constraintdefinition/validator/SerialNumber.java
@@ -0,0 +1,37 @@
+/*
+ * Bean Validation: constrain once, validate everywhere.
+ *
+ * License: Apache License, Version 2.0
+ * See the license.txt file in the root directory or <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
+package org.beanvalidation.specexamples.constraintdefinition.validator;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE_USE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Documented
+@Constraint(validatedBy = SerialNumberValidator.class)
+@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
+@Retention(RUNTIME)
+public @interface SerialNumber {
+
+	int length();
+
+	String message() default "{com.acme.constraint.SerialNumber.message}";
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+}
