07dab04ebb4320c1303f92c547ea341b35828ce5
Gunnar Morling <gunnar.morling@googlemail.com>
2017-06-21 17:47:31 +0200

    Misc. minor improvements

--- spec-examples/src/test/java/org/beanvalidation/specexamples/constraintdefinition/annotation/ELAssert.java
+++ spec-examples/src/test/java/org/beanvalidation/specexamples/constraintdefinition/annotation/ELAssert.java
@@ -12,6 +12,7 @@ import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.ElementType.TYPE_USE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Documented;
@@ -29,7 +30,7 @@ import javax.validation.Payload;
  */
 @Documented
 @Constraint(validatedBy = ELAssertValidator.class)
-@Target({ METHOD, FIELD, TYPE, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER })
+@Target({ METHOD, FIELD, TYPE, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
 @Retention(RUNTIME)
 public @interface ELAssert {
 
